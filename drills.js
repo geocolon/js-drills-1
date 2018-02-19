931e38b954231316c090c42be90d7200c1a89f62
George Colon <geocolon@gmail.com>
2018-02-19 13:35:57 -0800

    adding year of birth

--- drills.js
+++ drills.js
@@ -1,4 +1,6 @@
 function whoAmI(name, age){
+    let yearOfBirth = 2018 - age;
     console.log(`Hi my name is ${name} and I'm ${age} years old.`);
+    console.log(`I was born in ${yearOfBirth}`);
 }
 whoAmI("Alexa", 25);
