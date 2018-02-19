0e728acfb65298b30a7ba542e51cc0c8bebcaffa
George Colon <geocolon@gmail.com>
2018-02-19 13:59:01 -0800

    added try and catch

--- drills.js
+++ drills.js
@@ -1,9 +1,19 @@
 function yearOfBirth(age) {
-    return 2018 - age;
+    try {
+        yearOfBirth(-25);
+    }
+    catch (e){
+     if (age < 0){
+        throw new Error ('Age can not be a negative');
+        } else {
+         return 2018 - age;
+        }
+    }   
 }
 
 
 function whoAmI(name, age){
+    
     let yob = yearOfBirth(age);
     console.log(`Hi my name is ${name} and I'm ${age} years old.`);
     console.log(`I was born in ${yob}`);
