3099ecd9f9124208349fa3d9cbe3719bf837ddda
Alexa Scott <arscott4792@gmail.com>
2018-02-19 13:41:51 -0800

    added yearOfBirth function

--- drills.js
+++ drills.js
@@ -1,6 +1,13 @@
-function whoAmI(name, age){
-    let yearOfBirth = 2018 - age;
-    console.log(`Hi my name is ${name} and I'm ${age} years old.`);
-    console.log(`I was born in ${yearOfBirth}`);
+function yearOfBirth(age) {
+    return 2018 - age;
 }
+
+
+function whoAmI(name, age){
+    let yob = yearOfBirth(age);
+    console.log(`Hi my name is ${name} and I'm ${age} years old.`);
+    console.log(`I was born in ${yob}`);
+}
+
+
 whoAmI("Alexa", 25);
