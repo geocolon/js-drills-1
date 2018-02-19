2bc0796f0584cad9e3d73cae61f901e5f69f0578
Alexa Scott <arscott4792@gmail.com>
2018-02-19 14:06:46 -0800

    checked if undefined

--- drills.js
+++ drills.js
@@ -13,11 +13,15 @@ function yearOfBirth(age) {
 
 
 function whoAmI(name, age){
-    
+    if(name === undefined && age === undefined || typeof age !== "number") {
+        console.error("Arguments not valid.");
+    }
+
+
     let yob = yearOfBirth(age);
     console.log(`Hi my name is ${name} and I'm ${age} years old.`);
     console.log(`I was born in ${yob}`);
 }
 
 
-whoAmI("Alexa", 25);
+whoAmI("Alexa", "string");
